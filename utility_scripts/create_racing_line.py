5f56c93a64b75a6cd3a6b6928575f9c1759c8db2
Rahul Menon <rmeno12@utexas.edu>
2022-05-17 10:15:42 -0500

    use file for racing lines

--- utility_scripts/create_racing_line.py
+++ utility_scripts/create_racing_line.py
@@ -1,12 +1,33 @@
+"""
+This script creates a .navigation.json file for a map given a racing line description.
+The description of the racing line is a text file where each line is a point on the racing
+line and contains information about max speed on the upcoming segment. 
+
+The following example racing line means from (1, 2) to (4, 5) the speed limit is 3, from
+(4, 5) to (7, 8) the speed limit is 1, and from (7, 8) to (1, 2) the speed limit is 2.
+
+Example:
+1 2 3
+4 5 1
+7 8 2
+
+To use this script run `python create_racing_line <racing_line_file> > <.navigation.json_file>`
+"""
 import json
+import sys
+
+if len(sys.argv) != 2:
+    sys.stderr.write("Provide the filename of the racing line descriptor")
+
+with open(sys.argv[1]) as f:
+    lines = [line.split() for line in f.readlines()]
+    points = [(float(x), float(y)) for x, y, _ in lines]
+    speeds = [float(s) for _, _, s in lines]
 
 graph = {"edges": [], "nodes": []}
-points = [(10.0, 0.5), (20.7, 0.8), (21.2, 1.7), (22, 3.7), (21.4, 6.8), (20.4, 7.5), (19.75, 7.1), (19.4, 6.3), (20, 4.6), (20.1, 3.5), (19.3, 2.7), (17.9, 2.0), (11.8, 1.5), (11, 1.9), (10.7, 2.3), (10.9, 2.8), (11.5, 3.2), (17.4, 3.9), (18.1, 4.9), (17.9, 5.6), (17.5, 5.9), (16.6, 5.8), (15, 5.3), (1.3, 6.5), (0.75, 6), (0.8, 5.2), (1.2, 3.3), (1.6, 2.6), (2.7, 2.3), (7.5, 2.1), (8.9, 0.8)]
 
-id_ct = 0
 for p in points:
-    graph["nodes"].append({"id": id_ct, "loc": {"x": p[0], "y": p[1]}})
-    id_ct += 1
+    graph["nodes"].append({"id": len(graph["nodes"]), "loc": {"x": p[0], "y": p[1]}})
 
 for i in range(len(graph["nodes"]) - 1):
     graph["edges"].append({
@@ -15,7 +36,7 @@ for i in range(len(graph["nodes"]) - 1):
         "has_elevator": False,
         "has_stairs": False,
         "max_clearance": 10.0,
-        "max_speed": 50.0,
+        "max_speed": speeds[i],
         "s0_id": graph["nodes"][i]["id"],
         "s1_id": graph["nodes"][i+1]["id"],
     })
@@ -27,7 +48,7 @@ if len(graph["nodes"]) > 1:
         "has_elevator": False,
         "has_stairs": False,
         "max_clearance": 10.0,
-        "max_speed": 50.0,
+        "max_speed": speeds[len(graph["nodes"])-1],
         "s0_id": graph["nodes"][len(graph["nodes"])-1]["id"],
         "s1_id": graph["nodes"][0]["id"],
     })
